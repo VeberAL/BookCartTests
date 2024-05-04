f1644856ba388166923a2ea13920d8ce0b92862a
VeberAL <aleo83@rambler.ru>
2024-05-05 01:50:13 +0500

    Изменить авторизацию для аннотации WithLogin

--- src/test/java/helpers/LoginExt.java
+++ src/test/java/helpers/LoginExt.java
@@ -1,46 +1,26 @@
 package helpers;
 
-import io.restassured.response.Response;
+import data.DataTest;
+import models.LoginBodyModel;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.openqa.selenium.Cookie;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static api.ApiAuth.successfulAuthorisation;
 import static io.qameta.allure.Allure.step;
-import static io.restassured.RestAssured.given;
-import static specs.Spec.authResponseSpec;
-import static specs.Spec.userRequestSpec;
 
 public class LoginExt implements BeforeEachCallback {
 
 
-    private static Response authorizationResponse;
+    private static LoginBodyModel authorizationResponse;
 
     public void beforeEach(ExtensionContext context) {
-        String authData = "{\"userName\":\"VeberAL\",\"password\":\"12832156aA!\"}";
+        DataTest dataTest = new DataTest();
+        step("Авторизация пользователя.", () ->
+                authorizationResponse = successfulAuthorisation(dataTest.login, dataTest.password));
 
-        Response authResponse = step("Авторизация пользователя", () ->
-                given(userRequestSpec)
-                        .body(authData)
-                        .when()
-                        .post("/Account/v1/Login")
-                        .then()
-                        .spec(authResponseSpec)
-                        .extract().response());
-        authorizationResponse = authResponse;
-
-        String userID = authResponse.path("userId");
-        String expires = authResponse.path("expires");
-        String token = authResponse.path("token");
-
-        step("Передача cookie", () -> open("/favicon.ico"));
-        getWebDriver().manage().addCookie(new Cookie("userID", userID));
-        getWebDriver().manage().addCookie(new Cookie("expires", expires));
-        getWebDriver().manage().addCookie(new Cookie("token", token));
     }
 
-    public static Response Authorization() {
+    public static LoginBodyModel Authorization() {
         return authorizationResponse;
 
     }
