ec5f7f4494431dfa2a68901eaa256b3998391d15
VeberAL <aleo83@rambler.ru>
2024-05-05 01:13:09 +0500

    Добавить faker для генерации isbn книг

---
+++ src/test/java/data/DataFaker.java
@@ -0,0 +1,13 @@
+package data;
+
+import com.github.javafaker.Faker;
+
+public class DataFaker {
+    public String getBookId() {
+        String[] bookId = {"9781491904244", "9781491950296",
+                "9781593275846", "9781593277574",
+                "9781449331818", "9781449337711",
+                "9781449365035", "9781449325862"};
+        return new Faker().options().option(bookId);
+    }
+}
