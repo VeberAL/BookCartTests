beb2b01b41619c0f8abda65c5e1d538270711cab
VeberAL <aleo83@rambler.ru>
2024-05-05 20:36:24 +0500

    Убрать передачу cookies в класс авторизации

--- src/test/java/helpers/LoginExt.java
+++ src/test/java/helpers/LoginExt.java
@@ -1,23 +1,31 @@
 package helpers;
 
 import data.DataTest;
+import io.qameta.allure.Step;
 import models.ResLoginModel;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.openqa.selenium.Cookie;
 
 import static api.ApiAuth.successfulAuthorisation;
-import static io.qameta.allure.Allure.step;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class LoginExt implements BeforeEachCallback {
 
 
-    private static ResLoginModel authorizationResponse;
+    public static ResLoginModel authorizationResponse;
 
+    @Step("Авторизация пользователя.")
     public void beforeEach(ExtensionContext context) {
         DataTest dataTest = new DataTest();
-        step("Авторизация пользователя.", () ->
-                authorizationResponse = successfulAuthorisation(dataTest.login, dataTest.password));
+        authorizationResponse = successfulAuthorisation(dataTest.login, dataTest.password);
 
+        open(baseUrl + "/favicon.png");
+        getWebDriver().manage().addCookie(new Cookie("userID", authorizationResponse.getUserId()));
+        getWebDriver().manage().addCookie(new Cookie("expires", authorizationResponse.getExpires()));
+        getWebDriver().manage().addCookie(new Cookie("token", authorizationResponse.getToken()));
     }
 
     public static ResLoginModel Authorization() {
